781dc93b0fd12fa82591aa85e0445c95e2a4e948
Worrick <Wayne@waynesmbp.lan>
2014-08-15 12:23:11 +0800

    seems correct now
    hasn’t adapt to the fstream

--- Prob77/main.cpp
+++ Prob77/main.cpp
@@ -1,18 +1,54 @@
-//
-//  main.cpp
-//  Prob77
-//
-//  Created by Wayne Wang on 8/15/14.
-//  Copyright (c) 2014 Wayne. All rights reserved.
-//
+/*
+ ID: dearxia1
+ PROG: gift1
+ LANG: C++
+ */
 
 #include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
+using namespace std;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+//    ofstream fout ("gift1.out");
+//    ifstream fin ("gift1.in");
+	int NP, Spent[20] = {}, Revenue[20] = {}, Receivers, gift, s = 0;
+	char name[20][20] = {}, temp[20] = {};
+	cin >> NP;
+	for (int i = 0; i < NP; i++)
+        cin >> name[i];
+    for (int i = 0; i < NP; i++)
+    {
+        cin >> temp;
+        for (int j = 0; j < NP; j++)
+        {
+            if (!strcmp(temp, name[j]))
+            {
+                s = j;
+            }
+        }
+        cin >> Spent[s] >> Receivers;
+        if (Receivers)
+            gift = Spent[s] / Receivers;
+        else
+            gift = 0;
+        Spent[s] = gift * Receivers;
+        for (int j = 0; j < Receivers; j++)
+        {
+            cin >> temp;
+            for (int k = 0; k < NP; k++)
+            {
+                if (!strcmp(temp, name[k]))
+                {
+                    Revenue[k] += gift;
+                    break;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < NP; i++)
+        cout << name[i] << ' ' << Revenue[i] - Spent[i] << '\n';
+	return 0;
 }
-
